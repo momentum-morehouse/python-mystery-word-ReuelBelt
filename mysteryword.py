b6e41f988bbc0d47f2bcaf54b67a6bb61cfa3594
Reuel Belt <reuelbelt@gmail.com>
2020-09-30 17:10:57 -0700

    wordz2

--- mysteryword.py
+++ mysteryword.py
@@ -1,33 +1,41 @@
-# open a file on the server - in our example "words.txt"
+#$open a file on the server - in our example "words.txt"
 # with open("words.txt", "r") as file:
 #     wordz = file.read().splitlines()
 # print(wordz)
 
-# for version 2.0!!
+# for version 2.0!!!!!
 # select a random word (line) on the server 
+#$if input is more than 2 character limit only respond with Nope try again
 
-# this will come for version 2.0!!
+# this will come for version 2.0!!!!!!
 # user choose level of difficulty
 # easy 4-6 characters if/then statments
 # normal 6-8 characters if/then statements
 # hard 8+ characters if/then statments
 
-#select word to get started = pizza for 
+#$select word to get started = "pizza" for the test 
 word = "pizza"
 print(len(word))
 
-#show in console the number of letters in computers' word and display 
+#show in console the number of letters in computers' word 
 
-#user enter guess - one letter only (uppercase/lowercase) at a time per round
+#user enter guess - one letter only (uppercase/lowercase), at a time per round
+#set limit to input to 1 character
+#or else tell user input invalid
+
 def playgame():
     guess = input("Choose a letter ")
     print(guess)
     #let user know true or false
-    if guess in word:
+    if len(guess) > 1:
+        print("Try again")
+    elif guess in word:
         print("You guessed right!")
+        playgame()
     else:
         print("Nope, try again")
         playgame()
+    
 playgame()     
 
 #put guesses in list and count
